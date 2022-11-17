e37d614e313650d73a3a349590b90af8e8ff16b6
Les Prodiges <lesprodiges@MAC-GEN-C122-21.local>
2022-11-17 15:34:02 +0100

    Apres midi jour 4

--- symfony/src/Controller/ItemController.php
+++ symfony/src/Controller/ItemController.php
@@ -35,4 +35,21 @@ class ItemController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route('/item/{id}', name: 'app_item_show', methods: ['GET'])]
+    public function show(Item $item) {
+        return $this->render('item/show.html.twig', [
+            'item' => $item,
+        ]);
+    }
+
+    #[Route('/item/{id}/delete', name: 'app_item_delete', methods: ['POST'])]
+    public function delete(Request $request, Item $item, ItemRepository $itemRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$item->getId(), $request->request->get('_token'))) {
+            $itemRepository->remove($item, true);
+        }
+
+        return $this->redirectToRoute('app_order_show', ['id' => $item->getCommande()->getId()], Response::HTTP_SEE_OTHER);
+    }
 }
